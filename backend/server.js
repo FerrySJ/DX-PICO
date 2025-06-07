35e761c60f7fbb1b9eea7e1311ce6dddbacfb310
fern <nht.devops@gmail.com>
2025-06-07 15:10:54 +0700

    port 5000

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 app.use("/api/pico_setting", require("./api/pico_setting"));
 app.use("/api/pico_status", require("./api/pico_status"));
