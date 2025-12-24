65d7fd3f9b2f75c12ad9b6959f33f803d98de30e
fern <nht.devops@gmail.com>
2025-12-24 17:02:47 +0700

    add server mbr

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 5000;
 app.use("/api/pico_setting", require("./api/pico_setting"));
 app.use("/api/pico_status", require("./api/pico_status"));
 app.use("/api/pico_daily_report", require("./api/pico_daily_report"));
+// MBR NHT
+app.use("/api/mbr_setting", require("./api/mbr_setting"));
+app.use("/api/mbr_status", require("./api/mbr_status"));
+app.use("/api/mbr_daily_report", require("./api/mbr_daily_report"));
 
 
 app.listen(port, () => {
